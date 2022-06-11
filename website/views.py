036aa215720d14cb0430b13e1c28077ff6cd4a16
sai-parasad-1 <sai.prasad.1603@gmail.com>
2022-06-11 17:36:16 +1000

    Added testimonials Section

--- website/views.py
+++ website/views.py
@@ -1,11 +1,28 @@
 from http.client import HTTPResponse
+from multiprocessing import context
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    context = {'Testimonials': [
+        {'name': 'John Doe',
+         'role': 'Student',
+         'department': 'Department Of Electrical and Electrical Engineering',
+         'quote': ' A set of words that is complete in itself, typically containing a subject and predicate, conveying a statement, question, exclamation, or command. ',
+         'image': 'https://www.w3schools.com/howto/img_avatar.png',
+         'batch': '2019-2020'},
+
+
+        {'name': 'John Doe',
+         'role': 'Student',
+         'department': 'Department Of Electrical and Electrical Engineering',
+         'quote': 'I am a student of Electrical and Electrical Engineering at the University of Pretoria. I am a very active student and I am very happy to be a part of the team that is working on the website.',
+         'image': 'https://www.w3schools.com/howto/img_avatar2.png',
+         'batch': '2019-2020'},
+    ], "name": "John Doe", }
+    return render(request, 'home.html', context=context)
 
 
 def academics_page(request):
