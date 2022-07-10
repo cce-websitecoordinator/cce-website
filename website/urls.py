f3749bac706359e958f76c7900c1db703dc2a202
sai-parasad-1 <sai.prasad.1603@gmail.com>
2022-07-11 04:22:33 +1000

    added departments Routes in Navbar

--- website/urls.py
+++ website/urls.py
@@ -4,10 +4,15 @@ from website import views
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('academics', views.academics_page, name='academics'),
-    path('departments', views.departments_page, name='departments'),
     path('campuslife', views.campuslife_page, name='campuslife'),
     path('about', views.about_page, name='about'),
     path('nirf', views.nirf_page, name='nirf'),
     path('gallery', views.gallery_page, name='gallery'),
+    path('departments/BSH', views.department_of_BSH_page, name='Department of BSH'),
+    path('departments/CSE', views.department_of_CSE_page, name='Department of CSE'),
+    path('departments/CE', views.department_of_CE_page, name='Department of CE'),
+    path('departments/ME', views.department_of_ME_page, name='Department of ME'),
+    path('departments/EEE', views.department_of_EEE_page, name='Department of EEE'),
+    path('departments/ECE', views.department_of_ECE_page, name='Department of ECE'),
 
 ]
--- website/views.py
+++ website/views.py
@@ -27,8 +27,22 @@ def academics_page(request):
     return render(request, 'academics.html')
 
 
-def departments_page(request):
-    return render(request, 'departments.html')
+def department_of_BSH_page(request):
+    return render(request, 'Departments/index.html')
+
+def department_of_CSE_page(request):
+    return render(request, 'Departments/index.html')
+
+def department_of_CE_page(request):
+    return render(request, 'Departments/index.html')
+def department_of_ME_page(request):
+    return render(request, 'Departments/index.html')
+
+def department_of_EEE_page(request):
+    return render(request, 'Departments/index.html')
+
+def department_of_ECE_page(request):
+    return render(request, 'Departments/index.html')
 
 
 def campuslife_page(request):
