7bb2f17513a151905594713a71a7cabdc101a76a
sai-prasad-1 <sai.prasad.1603@gmail.com>
2022-09-13 18:31:01 +0530

    postgress migration

--- cce/settings.py
+++ cce/settings.py
@@ -93,7 +93,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'dbtest', 
+    #     'USER': 'postgres', 
+    #     'PASSWORD': '1234',
+    #     'HOST': '127.0.0.1', 
+    #     'PORT': '5432',
+    # }
 }
 
 
