3052b9bcd11cd13de4eb0893236b2c3f221703f7
sai-prasad-1 <sai.prasad.1603@gmail.com>
2022-11-08 15:55:54 +0530

    Disable Hot Reloading

--- cce/settings.py
+++ cce/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_browser_reload',
+    # 'django_browser_reload',
     'website',
     'departments',
     'storages',
