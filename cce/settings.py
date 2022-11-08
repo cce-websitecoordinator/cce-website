70a17a4253c16ee0081f609ecc8d7849c40eb0ea
sai-prasad-1 <sai.prasad.1603@gmail.com>
2022-11-08 20:47:11 +0530

    IQAC Page & Governing Bodies Completed

--- cce/settings.py
+++ cce/settings.py
@@ -144,21 +144,8 @@ STATICFILES_DIRS = [
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
-NPM_BIN_PATH = shutil.which('npm')
-TAILWIND_APP_NAME = 'theme'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = '/media/'
-# AWS Confifugrations
-
 
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'cce-website-media'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-PUBLIC_MEDIA_LOCATION = 'media'
-AWS_LOCATION = ''
-print("AWS Configurations are set")
-DEFAULT_FILE_STORAGE = 'cce.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'cce.storage_backends.MediaStorage' 
--- website/urls.py
+++ website/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('about', views.about_page, name='about'),
     path('academics', views.academics_page, name='academics'),
     path('arts', views.arts_page, name='arts'),
+    path('governing_body', views.governing_body__page, name='governingBody'),
     path('campuslife', views.campuslife_page, name='campuslife'),
     path('nirf', views.nirf_page, name='nirf'),
     path('iqac', views.iqac_page, name='iqac'),
