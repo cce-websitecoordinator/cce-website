1f46534a80c096b227a11d031cea2493a7d2efb2
Amal Paul <amalpaulofficial@gmail.com>
2022-11-08 15:49:53 +0530

    Configure settings for production

--- cce/settings.py
+++ cce/settings.py
@@ -20,8 +20,8 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -137,9 +137,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Added by manually
 # custom settings
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+#    os.path.join(BASE_DIR, "static"),
 ]
 INTERNAL_IPS = [
     "127.0.0.1",
