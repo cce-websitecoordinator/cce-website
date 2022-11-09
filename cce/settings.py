959ddc940fb1388a67db7961aaff116c11e46f8f
AmalPaul <amalpaul20cs@cce.edu.in>
2022-11-09 15:33:41 +0530

    Add admission page

--- cce/settings.py
+++ cce/settings.py
@@ -20,8 +20,8 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
-# DEBUG = True
+# DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
