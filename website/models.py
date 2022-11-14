67cefdaa5397cc49ba9039ad11b659310930424c
sai-prasad-1 <sai.prasad.1603@gmail.com>
2022-11-14 10:04:59 +0530

    Configured Governing Bodies Model

--- website/models.py
+++ website/models.py
@@ -101,3 +101,11 @@ class AlumniCommittee(models.Model):
     role = models.CharField(max_length=100)
     def __str__(self):
         return self.name
+
+
+class GoverningBody(models.Model):
+    name = models.CharField(max_length=100)
+    designation = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
