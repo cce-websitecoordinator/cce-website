11f09eab33565b5bf66a0b3615b4770b2888080f
sai-prasad-1 <sai.prasad.1603@gmail.com>
2023-01-15 14:58:41 +0530

    Added Derpartment Research Pages

--- departments/urls.py
+++ departments/urls.py
@@ -6,10 +6,8 @@ from . import views
 urlpatterns = [
     path('professionalbodies/<str:slug>', views.ProfessionalBodie, name='dep_professionalbodies'),
     path('associations/<str:slug>', views.Association, name='dep_associations'),
+    path('research/<str:department>/<str:slug>', views.research_page, name='dep_research'),
     path('<str:department>/<str:route>', views.Department, name='Department'),
-
 ]
-
-
 if settings.DEBUG:     
      urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
