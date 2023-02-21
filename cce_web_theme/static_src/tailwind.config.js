5433623963270186cebe952a66ea255000c324ba
sai-prasad-1 <sai.prasad.1603@gmail.com>
2023-02-21 11:04:00 +0530

    Add Leaflet to Static

--- cce_web_theme/static_src/tailwind.config.js
+++ cce_web_theme/static_src/tailwind.config.js
@@ -2,7 +2,7 @@
  * This is a minimal config.
  *
  * If you need the full config, get it from here:
- * https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
+ * https://kg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
  */
  const Path = require("path");
  const pwd = process.env.PWD;
