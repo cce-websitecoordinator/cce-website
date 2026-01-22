8f3fe3fb1bfbc7706c455fe46c6dbaec8b258d65
Abhin_krishna <abhinkrishnamp0@gmail.com>
2026-01-22 12:50:10 +0530

    added department and changed academics

--- departments/views.py
+++ departments/views.py
@@ -26,10 +26,7 @@ def getDepartment(department):
             return "Civil Engineering"
         case "DS":
             return "Computer Science and Engineering (Data Science)"
-        case "BS":  # Match the code used in navbar.html
-            return "Department of Computer Science and Engineering (Business Systems)"
-        case "MBA": # Match the code used in navbar.html
-            return "Master of Business Administration"
+        
 
 
 class Context:
